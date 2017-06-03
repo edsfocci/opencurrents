dbfd28a042a605547d0a79357daab5ae64738026
karbmk <karthikbm01@gmail.com>
2017-06-02 22:56:44 -0700

    added testcase for user creation and login in the unit test framework of Django
    Signed-off-by: karbmk <karthikbm01@gmail.com>

--- openCurrents/tests.py
+++ openCurrents/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+######################################################################################
+#Run this file in the manage.py directory as 'python manage.py test openCurrents.tests'
+######################################################################################
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    
+    def test_login(self):
+        # send login data
+        form_data = { 'user_email':'testuser','user_password':'secret' }
+        response = self.client.post('/process_login/', form_data, follow=True)
+        self.assertContains(response, "Invalid login/password", 1, 200)
+        
+    def test_log_success(self):
+        form_data = { 'user_email':'testuser','user_password':'secret' }
+        response = self.client.post('/process_login/', form_data, follow=True)
+        print(response)
+        self.assertContains(response, "Welcome", 1, 200)
